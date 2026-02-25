725de1d4ced6d2d82f8cc0a57b493151e123a730
PriyanshiSri05 <priyanshi.sri603@gmail.com>
2026-02-25 10:14:50 +0530

    UC2: Print hardcoded palindrome result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,8 +2,25 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Application Started");
+        System.out.println("==================================");
+        System.out.println("      PALINDROME CHECKER APP      ");
+        System.out.println("==================================");
+        System.out.println("UC2: Print a Hardcoded Palindrome Result");
+        System.out.println("----------------------------------");
 
+        String word = "madam";   // Hardcoded string
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome
+        if (word.equals(reversed)) {
+            System.out.println("Result: " + word + " is a Palindrome");
+        } else {
+            System.out.println("Result: " + word + " is NOT a Palindrome");
+        }
     }
-
 }
