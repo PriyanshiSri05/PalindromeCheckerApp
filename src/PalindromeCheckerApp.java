6df6a787215f7667bcce271114319bb47edd0ca3
PriyanshiSri05 <priyanshi.sri603@gmail.com>
2026-03-05 09:50:03 +0530

    UC8 : Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,43 +1,90 @@
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
+    static class Node {
+        char data;
+        Node next;
 
-        System.out.println("==================================");
-        System.out.println("      PALINDROME CHECKER APP      ");
-        System.out.println("==================================");
-        System.out.println("UC7: Deque-Based Optimized Palindrome Checker");
-        System.out.println("----------------------------------");
-
-        String word = "radar";
-
-        Deque<Character> deque = new LinkedList<>();
-
-        // Insert characters into deque
-        for (int i = 0; i < word.length(); i++) {
-            deque.addLast(word.charAt(i));
+        Node(char data) {
+            this.data = data;
+            this.next = null;
         }
+    }
 
-        boolean isPalindrome = true;
+    public static Node createLinkedList(String str) {
+        Node head = null;
+        Node tail = null;
 
-        // Compare front and rear
-        while (deque.size() > 1) {
-            char front = deque.removeFirst();
-            char rear = deque.removeLast();
+        for (char ch : str.toCharArray()) {
+            Node newNode = new Node(ch);
 
-            if (front != rear) {
-                isPalindrome = false;
-                break;
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
         }
+        return head;
+    }
 
-        // Print result
-        if (isPalindrome) {
-            System.out.println("Result: " + word + " is a Palindrome");
-        } else {
-            System.out.println("Result: " + word + " is NOT a Palindrome");
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
         }
+        return prev;
+    }
+
+    public static boolean isPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node secondHalf = reverse(slow);
+        Node firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data)
+                return false;
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("PALINDROME CHECKER APP - UC8");
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        Node head = createLinkedList(input);
+
+        if (isPalindrome(head))
+            System.out.println("Result: " + input + " is a Palindrome");
+        else
+            System.out.println("Result: " + input + " is NOT a Palindrome");
+
+        scanner.close();
     }
 }
